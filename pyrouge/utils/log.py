5aad794b0b394b63cd9fc4d86b4813da1fc8366a
papagandalf <papagandalf@gmail.com>
2016-11-17 19:21:34 +0000

    enabled setting logging level when creating Rouge155 object

--- pyrouge/utils/log.py
+++ pyrouge/utils/log.py
@@ -1,17 +1,17 @@
 import logging
 
 
-def get_console_logger(name):
+def get_console_logger(name,level=logging.INFO):
     logFormatter = logging.Formatter(
         "%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
     logger = logging.getLogger(name)
     if not logger.handlers:
-        logger.setLevel(logging.INFO)
+        logger.setLevel(level)
         ch = logging.StreamHandler()
         ch.setFormatter(logFormatter)
         logger.addHandler(ch)
     return logger
 
 
-def get_global_console_logger():
-    return get_console_logger('global')
+def get_global_console_logger(level=logging.INFO):
+    return get_console_logger('global', level)
