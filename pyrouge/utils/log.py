2854dc938b6d3f3c8f9cb66c5d5bc43458ede095
Benjamin Heinzerling <bheinzerling@users.noreply.github.com>
2016-11-18 09:19:48 +0100

    Update log.py
    Fix formatting

--- pyrouge/utils/log.py
+++ pyrouge/utils/log.py
@@ -1,7 +1,7 @@
 import logging
 
 
-def get_console_logger(name,level=logging.INFO):
+def get_console_logger(name, level=logging.INFO):
     logFormatter = logging.Formatter(
         "%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
     logger = logging.getLogger(name)
